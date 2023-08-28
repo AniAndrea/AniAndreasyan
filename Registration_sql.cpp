95ea7a2fd2c8583329410a86faf8b7242ba3c66a
AniAndrea <135700881+AniAndrea@users.noreply.github.com>
2023-08-29 00:22:40 +0400

    Add files via upload
    all_is_done. version

--- Registration_sql.cpp
+++ Registration_sql.cpp
@@ -4,25 +4,27 @@
 #include <fstream>
 #include <string.h>
 #include <stdlib.h>
-#include <mysql.h>
 #include <cstdlib>
 #include <ctime>
+#include <cctype>
 //#include "utils/Utils.h"
 
 using namespace std;
 
-int  login();
+void  login();
 void forgot();
 void firstpage();
 void firstpage_two();
 void profile();
-int registration();
+void registration();
+bool lettersAndNumbers();
+string enterRestName();
+string enterAddress();
 string generateID();
-string RuserName;
+string RuserName, RestName, RPassword, Address;
 
 int  main()
 {
-//string RuserName;
 	cout<<"\t\t\t ___________________________________ \n";
 	cout<<"\t\t\t ***             WELCOME!        *** \n";
 	cout<<"\t\t\t    ______________________________   \n";
@@ -76,38 +78,72 @@ void firstpage_two() {
                 case 2:
                         cout<<"\t\t\t Thank you! \n\n";
 	      		break;
+        default:
+			cout<<"\x1B[2J\x1B[H";
+			cout<<"\t\t\t Select  from the options given above \n";
+			firstpage();
         }
 }
-
 
 void profile(){
 	cout<<"\t\t\t\t --------- Hello!--------- \n\n";
 	cout<<"\t\t\t\t  Here is your Profile \n\n\n"; }
 // ___________________________________________________________ //
 
+string enterRestName(){
+cout<<"\t\t\t Enter your public food place name: \n";
+cout<<"\t\t\t __ ";
+cin.ignore();
+getline(cin, RestName);
+return RestName;
+}
+
+string enterAddress(){
+cout<<"\t\t\t Enter address: \n";
+cout<<"\t\t\t __ ";
+cin.ignore();
+getline(cin, Address);
+return Address;
+}
+
 string generateID(){
 
     string ID;
 	srand(time(0));
-	int random=rand()%1000;
+	int random=(rand()%10000);
 	string str=to_string(random);
 	ID=RuserName+str;
-	//std::cout<<random<<std::endl;
 return ID;
+}
+
+bool lettersAndNumbers(string& RPassword){
+bool hasLetters=false;
+bool hasNumbers=false;
+for(char c: RPassword){
+    if(isalpha(c)){
+        hasLetters=true;
+    }
+    else if(isdigit(c)){
+        hasNumbers=true; }
+    if(hasLetters && hasNumbers){
+    break;
+    }
+}
+return hasLetters && hasNumbers;
 }
 // _________________________________________________________________ //
 
 
-int   registration()
+void  registration()
 {
-	string RestName, RPassword, Address, Email; 
-        int a;
-        char userrole[20]="User";
+	string Email; 
+    int a;
+    char userrole[20]="User";
 	cout<<"\x1B[2J\x1B[H";
         
 	cout<<"\t\t\t Create a USERNAME: "<<endl;
-        cout<<"\t\t\t _ ";
-        cin>>RuserName;
+    cout<<"\t\t\t _ ";
+    cin>>RuserName;
 // ************************ //
 	
 	char server[30] = "sql6.freesqldatabase.com";
@@ -119,51 +155,50 @@ int   registration()
 
     if (conn == NULL) {
         printf("MySQL initialization failed");
-        return 1;
-    }
+        return;}
 
     if (mysql_real_connect(conn, server, username, password, database, 0, NULL, 0) == NULL) {
         printf("Unable to connect with MySQL server\n");
         mysql_close(conn);
-        return 1;
-    }
+        return;}
 
     if(mysql_query(conn, "SELECT * FROM USER")) {
 	    cout<<"Unable to  connect with MySQL server\n";
 	    mysql_close(conn);
-	    return 1;}
-	
+	    return;}
 	
 	MYSQL_RES *result = mysql_store_result(conn);
     	if (result == NULL) {
-	 cout << "Result fetching error." <<endl;
-       mysql_close(conn);
-        return 1; 
-    }
+	    cout << "Result fetching error." <<endl;
+        mysql_close(conn);
+        return;}
+
 	 int num_fields = mysql_num_fields(result);
          MYSQL_ROW row;
     	 while ((row = mysql_fetch_row(result))) {
         	for (int i = 0; i < num_fields; ++i) {
             	if (RuserName==row[i]) {
                 cout << "\t\t\t This username already exist! Try again! "<<endl;
-		cout<<"\t\t\t __ ";
-		cin>>RuserName;
+	            cout<<"\t\t\t __ ";
+		        cin>>RuserName;
             	}
+            }   
         }
-    }
 
     mysql_free_result(result);
     mysql_close(conn);
         	
 // ************************* //
-	cout<<"\n\n\t\t\t Enter your public food place name: "<<endl;
-        cout<<"\t\t\t _ ";
-        cin>>RestName;
+	// cout<<"\n\n\t\t\t Enter your public food place name: "<<endl;
+    // cout<<"\t\t\t _ ";
+    // getline(cin, RestName);
+    enterRestName();
 
-        cout<<"\n\n\t\t\t Create a PASSWORD: "<<endl;
-        cout<<"\t\t\t Your  password must  have letters, numbers and minimum 6 symbols "<< endl ;
-        cout<<"\t\t\t _ ";
-        cin>>RPassword;
+    cout<<"\n\n\t\t\t Create a PASSWORD: "<<endl;
+    cout<<"\t\t\t Your  password must  have letters, numbers and minimum 6 symbols "<< endl ;
+    cout<<"\t\t\t _ ";
+    cin>>RPassword;
+
 
         while  (RPassword.length() < 6 ) {
         cout<< "\t\t\t Your password must have minimum 6 symbols . Try again "<< endl ;
@@ -171,35 +206,24 @@ int   registration()
         cout<<"\t\t\t _ ";
         cin >> RPassword ;
         }
+        while(true){
+            if(lettersAndNumbers(RPassword)){
+            cout<<"\t\t\t Strong password! \n\n\n "; 
+            break; }
+            else{
+            cout<<"\t\t\t Password have to  contain both  letters and numbers. \n";
+            cout<<"\t\t\t __ ";
+            cin>>RPassword;}
+        }
 
-        cout<<"\n\n\t\t\t Enter your address: "<<endl;
-        cout<<"\t\t\t _ ";
-        cin>>Address;
-
-        cout<<"\n\n\t\t\t Enter your email: "<<endl;
-        cout<<"\t\t\t _ ";
-        cin>>Email;
-
-
-
-
-	//UserRole();
-// ____________________________________ //
-/*
-        cout<<"\t * Press 1 to for ADMIN:    "<<endl;
-        cout<<"\t * Press 2 to USER: "<<endl;
-        cout<<"\t __ ";
-        cin>>a;
-
-        switch(a)
-        {
-                case 1:
-                        userrole="Admin";
-                        break;
-                case 2:
-                        userrole="User";
-                        break;
-         }*/
+    // cout<<"\n\n\t\t\t Enter your address: "<<endl;
+    // cout<<"\t\t\t _ ";
+    // getline(cin, Address);
+    
+    enterAddress();
+    cout<<"\n\n\t\t\t Enter your email: "<<endl;
+    cout<<"\t\t\t _ ";
+    cin>>Email;
 // _________________________________________ //
 
 	cout<<"\x1B[2J\x1B[H";
@@ -209,13 +233,13 @@ int   registration()
 
     if (connn == NULL) {
         printf("MySQL initialization failed");
-        return 1;
+        return;
     }
 
     if (mysql_real_connect(connn, server, username, password, database, 0, NULL, 0) == NULL) {
         printf("Unable to connect with MySQL server\n");
         mysql_close(connn);
-        return 1;   
+        return;   
     }
 
   /*  if (mysql_query(conn, "CREATE TABLE USER(Eid INT PRIMARY KEY AUTO_INCREMENT, Ename VARCHAR(16), Salary INT)")) {
@@ -224,35 +248,28 @@ int   registration()
         return 1;
     }*/
     string userID=generateID();
-    cout<<userID<<endl;
     //string userID = generateUniqueID();
 
     string queryString = "INSERT INTO USER(ID, Username, RestName, Password, Address, Email, MenuID, UserRole) VALUES ('" + userID +  "','" + RuserName + "','" + RestName +"','" + RPassword + "','" + Address + "','" + Email + "','0','" + userrole + "')";
     string insertQuery = queryString;
     if (mysql_query(connn, insertQuery.c_str())) {
-        // Handle query execution error
         mysql_close(connn);
-        return 1;
+        return;
     } 
 
 		cout<<"\t\t\t Now you  can login to your profile. \n\n\n";
-                firstpage();
+            firstpage();
 
 }
 
 // ________________________________________________________ //
 
 
-int login()
-{
+void login() {
 	int count;
 	string UserName, Password;
         	
 	cout<<"\x1B[2J\x1B[H";
-	cout<<"\t\t\t Enter the USERNAME: ";
-	cin>>UserName;
-	cout<<"\t\t\t Enter the PASSWORD: ";
-    cin>>Password;
 
 // *************************************** //
 	char server[30] = "sql6.freesqldatabase.com";
@@ -264,63 +281,61 @@ int login()
 
     if (conn == NULL) {
         printf("MySQL initialization failed");
-        return 1;
+        return;
     }
 
     if (mysql_real_connect(conn, server, username, password, database, 0, NULL, 0) == NULL) {
         printf("Unable to connect with MySQL server\n");
         mysql_close(conn);
-        return 1;
+        return;
     }
+
+    bool exist = false;
+    MYSQL_RES *result = mysql_store_result(conn);
+    MYSQL_ROW row;
+    while(!exist) {
+        cout<<"\t\t\t Enter username: \n";
+        cout<<"\t\t\t __ ";
+        cin >> UserName;
+        cout<<"\t\t\t Enter password: \n";
+        cout<<"\t\t\t __ ";
+        cin >> Password;
+        string selectAllQuery = "SELECT * FROM USER";
 
     if(mysql_query(conn, "SELECT * FROM USER")) {
             cout<<"Unable to  connect with MySQL server\n";
             mysql_close(conn);
-            return 1;}
+            return;}
 	
 	MYSQL_RES *result = mysql_store_result(conn);
         if (result == NULL) {
          cout << "Result fetching error." <<endl;
        	mysql_close(conn);
-        return 1;
+        return;
+        }
+ while ((row = mysql_fetch_row(result))) {
+            if (UserName == row[1] && Password == row[3]) {
+                exist = true;
+                cout <<"\n\n\t\t\t\t Welcome to your profile! \n\n\n";
+                profile();
+                break;
+            }
+            else {
+                exist = false;
+            }
+        }
+        if(exist) {
+            mysql_free_result(result);
+            mysql_close(conn);
+            break;
+        }
+        else {
+            std::cout <<"Username or password is incorrect!. Try again! \n\n";
+            continue;
+        }
     }
-	
-
-    int num_fields = mysql_num_fields(result);
-         MYSQL_ROW row;
-    	while ((row = mysql_fetch_row(result))) {
-            string rowUsername = row[1];
-         string rowPassword = row[3];
-            	if (rowUsername.compare(UserName)==0){
-                    if(rowPassword.compare(Password)==0){
-                    cout<<"\n\n\t\t\tWelcome to your profile! \n\n";
-                    profile();
-                        }
-                else{ 
-                cout<<"\x1B[2J\x1B[H";
-                cout<<"\n\n\n\t\t\t Incorrect username or password! Try again! \n\n";
-                firstpage_two(); }                
-                }                       
-       }
- 
-/*
-	int num_fields = mysql_num_fields(result);
-	MYSQL_ROW row;
-	while ((row = mysql_fetch_row(result))) {
-	string rowUsername = row[1];
-        string rowPassword = row[3];
-        if(rowUsername.compare(UserName) == 0 && rowPassword.compare(Password) == 0){
-             cout<<"\n\t\t\t Welcome to  your profile! \n\n"<<endl;
-	         profile();  }
-	      
-	     else { 
-	cout<< "\t\t\t Incorrect!. Try again! \n "<<endl;
-	firstpage_two(); }
-	}
-*/
-
-    mysql_free_result(result);
-    mysql_close(conn);
+    //mysql_free_result(result);
+    //mysql_close(conn);
 }
 // ******************************************* //
 	
@@ -369,16 +384,5 @@ void forgot()
 			cout<<"\t\t Wrong choice! Try  again!";
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
